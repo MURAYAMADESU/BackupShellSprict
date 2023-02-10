75ef12336b996c0c2613290da33a98d78be56dbf
MURAYAMADESU <kousuke_21n1200380@nnn.ed.jp>
2023-02-11 03:12:11 +0900

    複数のパッケージを検索後自動インストール可能にしました。

--- autoBackupSprict.sh
+++ autoBackupSprict.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 #バックアップ元の絶対パス
-BackupSource="/TestingSpace/backupSource/"
+BackupSource="/TestingSpace/autoBackupSprict/backupSource"
 #バックアップ先の絶対パス
-BackupDestination="/TestingSpace/backupDestination"
+BackupDestination="/TestingSpace/autoBackupSprict/backupDestination"
 #逆増分バックアップによって消去されたファイルの保存場所
-tmp="/TestingSpace/tmp"
+tmp="/TestingSpace/autoBackupSprict/tmp"
 
-dpkg -s rsync>/dev/null 2>&1
+#必要パッケージの確認とインストール
+apt-get update>/dev/null
+
+package=("rsync" "tree")
+for serch in "${package[@]}"
+do
+
+dpkg -s "$serch">/dev/null 2>&1
 if [ ! $? -eq 0 ]; then
-apt install -y rsync
+apt install -y "$serch"
 fi
 
-rsync -av --delete  --backup --backup-dir='/TestingSpace/autBackupSprict/tmp/deletedata-'$(date +%Y'/'%m'/'%d-%H:%M:%S) ./backupSource/ ./backupDestination/
+done
+
+#バックアップ
+rsync -av --delete  --backup --backup-dir='.tmp/deletedata-'$(date +%Y'/'%m'/'%d-%H:%M:%S) "$BackupSource" "$BackupDestination"
